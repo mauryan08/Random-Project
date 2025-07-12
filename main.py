43c6fb12509df323480ed21df27ad13b72662f3e
Rupali Maurya <rupali.maurya08@gmail.com>
2025-07-13 00:02:07 +0530

    Added base files

---
+++ main.py
@@ -0,0 +1,8 @@
+import other_func
+
+print("Hello World !")
+
+ans_1 = other_func.add_nums(2, 3)
+ans_2 = other_func.subtract_nums(1, 5)
+
+print(ans_1, ans_2)
---
+++ other_func.py
@@ -0,0 +1,8 @@
+def add_nums(a, b):
+
+    return a + b
+
+
+def subtract_nums(a, b):
+
+    return a - b
